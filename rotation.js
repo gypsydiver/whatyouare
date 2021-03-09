13e023b9d02dbce5a598bf2ff0150e2058e9b448
Fernando Mendoza <fermendozarcs@gmail.com>
2021-03-09 16:34:14 +0100

    add color and word rotation

---
+++ rotation.js
@@ -0,0 +1,33 @@
+window.onload = pre_loader;
+
+backgrounds = [
+"#FAE8DA",
+"#C47684",
+"#EBD1EC",
+"#FFB68C",
+"#C7DAC7",
+"#E5F8FC",
+"#FAFFC7",
+"#D7EEFF",
+"#CCF1FF",
+"#E0D7FF",
+"#FFCCE1",
+"#D7E2FA",
+"#B8D6F2",
+"#DDC3E0",
+"#EDCCE5",
+"#F5D7E2",
+"#F7E7DE",
+"#C6CEFA"
+]
+
+words = ["beautiful", "fierce", "dazzling"]
+
+function pre_loader() {
+  javascript:document.getElementById('body').style.backgroundColor=randomSelection(backgrounds);
+  javascript:document.getElementById('msg').innerText = "You are " + randomSelection(words) + ".";
+}
+
+function randomSelection(collection) {
+  return collection[Math.floor(Math.random() * collection.length)]
+}
